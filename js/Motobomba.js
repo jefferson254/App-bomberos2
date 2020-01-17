359c9fd9885a0ebb2f80ade0efc31aede0e5d655
jefferson zambrano <jefferwell2522@gmail.com>
2020-01-17 00:31:23 -0500

    App Bomberos

--- js/Motobomba.js
+++ js/Motobomba.js
@@ -1,50 +1,51 @@
 class Motobomba { 
-constructor(id,capacidad,estado=false){
-    this.id=id;
-    this.capacidad=capacidad;
-    this.estado=estado;
-}
-prender(){ 
-this.estado=true;
-}
-apagar(){ 
-this.eatado=false;
-
-}
-getEstado(){
-   return this.estado;
-}
-toString(){
-    if(this.estado == true){
-    return  (this.estado + " " + "estado prendido")
-}else{
-    return "la motobomba con id " + this.id + " esta prendido"
-}
+    constructor(id,capacidad,estado=false){
+        this.id=id;
+        this.capacidad=capacidad;
+        this.estado=estado;
     }
-}
-
-function guardar(){
-    var  recuperar= document.getElementById('id').value;
-    var  recu1= document.getElementById('capacidad').value;
-this.id=recuperar;
-this.capacidad=recu1;
-console.log(this.id)
-console.log(this.capacidad)
-document.getElementById("demo").innerHTML ='El ID es: '+recuperar+' '+" "+'Capacidad es:  '+ recu1;
-
-document.getElementById("imagen").innerHTML = ('<img src="https://static.vecteezy.com/system/resources/previews/000/109/957/non_2x/vector-fire-truck.jpg"/>');
-
-let boton = document.createElement("button");
-boton.innerHTML = "Ver el estado";
-document.body.appendChild(boton);
-
-boton.onclick = function(){
-    if(this.estado == true){
-        return boton.innerHTML='Apagado';
-    }else{
-        return boton.innerHTML='Encendido';
-   
+    prender(){ 
+    this.estado=true;
+    }
+    apagar(){ 
+    this.eatado=false;
     
-}
-}
-}
+    }
+    getEstado(){
+       return this.estado;
+    }
+    toString(){
+        if(this.estado == true){
+            document.getElementById('estado').innerHTML='<img src="img/azul.jpg" windth="130px" height="100px">'
+        var parrafo=document.createElement('li')
+            parrafo.innerHTML = 'Esta Encendido'
+            document.body.appendChild(parrafo)
+            carro.apagar()
+            document.getElementById("boton").innerHTML= "Encendido"
+    }
+    if(this.estado == false){
+        document.getElementById('estado').innerHTML='<img src="img/rojo.jpg" windth="130px" height="100px">'
+    var parrafo=document.createElement('li')
+        parrafo.innerHTML = 'Esta Apagado'
+        document.body.appendChild(parrafo)
+        carro.encendido()
+        document.getElementById("boton").innerHTML= "Apagado"
+        }
+    }
+    
+     guardar(){
+        var  recuperar= document.getElementById('id').value;
+        var  recu1= document.getElementById('capacidad').value;
+    this.id=recuperar;
+    this.capacidad=recu1;
+    console.log(this.id)
+    console.log(this.capacidad)
+    document.getElementById("demo").innerHTML ='El ID es: '+recuperar+' '+" "+'Capacidad es:  '+ recu1;
+    
+    
+  
+       
+        
+    }
+    }
+    
